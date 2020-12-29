33bbd94e69d339aefe58cb3778621b4b08355dff
kevindavis-gd <kevindavis.gd@gmail.com>
2020-12-29 14:52:01 -0600

    Post Methods

--- src/main/java/com/SpringBootStarter/Topic/TopicController.java
+++ src/main/java/com/SpringBootStarter/Topic/TopicController.java
@@ -1,29 +1,40 @@
 package com.SpringBootStarter.Topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
+
 	@Autowired
 	private TopicService topicService;
 
+	// ***********************************************
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
-		}
-	
+	}// getAllTopics
+
+	// ************************************************
 	@RequestMapping("/topics/{id}")
-	//use the variable path named id and store it into the id variable
-	public Topic getTopic(@PathVariable("id") String id)
-	{
-		
+	// use the variable path named id and store it into the id variable
+	public Topic getTopic(@PathVariable("id") String id) {
+
 		return topicService.getTopic(id);
-	}
+	}// getTopic
+
+	// ***********************************************
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+		
+
+	}//addTopic
+
 }
--- src/main/java/com/SpringBootStarter/Topic/TopicService.java
+++ src/main/java/com/SpringBootStarter/Topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.SpringBootStarter.Topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,19 +9,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
+	private List<Topic> topics = new ArrayList<>( Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "JavaScript", "JavaScript Description"));
-
+			new Topic("javascript", "JavaScript", "JavaScript Description")));
+	
+	//***************************************************************
 	public List<Topic> getAllTopics()
 	{
 		return topics;
-	}
+	}//getAllTopics
 	
+	//***************************************************************
 	public Topic getTopic(String id)
 	{
 		// for each topic, check if t.geId is equal the id is passed in
 		// then return the topic
 		return topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+	}//getTopic
+	
+	//****************************************************************
+	
+	public void addTopic(Topic topic)
+	{
+		topics.add(topic);
 	}
+
+
+
+
 }
